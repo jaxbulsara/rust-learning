1f08ac7f585aa70d7cd413f8ead9c114762f5717
Jay Bulsara <jaybulsara@proton.me>
2023-01-16 11:07:18 -0500

    Completed 5.2 refactoring with structs.

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,13 +1,20 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     )
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    let (width, height) = dimensions;
-    width * height
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
